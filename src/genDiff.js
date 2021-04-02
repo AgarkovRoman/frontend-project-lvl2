f00c776cade23ac1d582074b28a0ac5c4202f091
Roman Agarkov <agarkovromachka@mail.ru>
2021-04-02 12:07:44 +0300

    add action

---
+++ src/genDiff.js
@@ -0,0 +1,14 @@
+import program from 'commander';
+import {readFile} from "./readFile.js";
+
+export function genDiff() {
+  program
+    .version('1.0.0')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1>, <filepath2>')
+    .action((path1, path2) => {
+      readFile(path1, path2)
+    })
+  program.parse();
+}
