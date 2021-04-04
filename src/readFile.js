4af74629b5def42a4b66e1e4560fc6cc693e39af
Roman Agarkov <agarkovromachka@mail.ru>
2021-04-04 23:10:02 +0300

    update file

--- src/readFile.js
+++ src/readFile.js
@@ -1,13 +1,10 @@
-import { readFileSync } from 'fs';
-import path from 'path';
 import getSortKeys from './utils/getSortKeys.js';
 import getDiffString from './getDiffString.js';
+import parsers from './parsers.js';
 
 export default function readFile(path1, path2) {
-  const file1 = readFileSync(path.resolve(process.cwd(), path1), 'utf-8');
-  const file2 = readFileSync(path.resolve(process.cwd(), path2), 'utf-8');
-  const parseFile1 = JSON.parse(file1);
-  const parseFile2 = JSON.parse(file2);
+  const parseFile1 = parsers(path1);
+  const parseFile2 = parsers(path2);
   const allSortKeys = getSortKeys(parseFile1, parseFile2);
   const result = getDiffString(allSortKeys, parseFile1, parseFile2);
   console.log(result);
